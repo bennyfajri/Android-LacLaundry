3ebe1fb69a0d7b93ac0c70500c3c37f1557017ed
bennyfajri <m.b.fajrii@gmail.com>
2021-06-04 22:31:26 +0700

    add ConfirmTransaction

---
+++ app/src/main/java/com/benny/laclaundry/ui/transaksi/KonfirmasiPesanan.kt
@@ -0,0 +1,12 @@
+package com.benny.laclaundry.ui.transaksi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.benny.laclaundry.R
+
+class KonfirmasiPesanan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_konfirmasi_pesanan)
+    }
+}
